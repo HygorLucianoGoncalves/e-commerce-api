e72dde3c1324939bd4e07cd5d8aba52403f8e703
Hygor Luciano <hygorluciano0@gmail.com>
2023-11-28 09:29:46 -0300

    Initial commit

---
+++ src/test/java/com/hygorluciano/lojaderoupa/LojaderoupaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hygorluciano.lojaderoupa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaderoupaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
