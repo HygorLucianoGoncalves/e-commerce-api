9b92327f3e0bc99f6369dd6dd6fb1b93f6f3193a
Hygor Luciano <hygorluciano0@gmail.com>
2023-12-02 10:24:41 -0300

    add metodo delete

--- src/main/java/com/hygorluciano/lojaderoupa/domain/controller/ProdutoController.java
+++ src/main/java/com/hygorluciano/lojaderoupa/domain/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import com.hygorluciano.lojaderoupa.domain.service.ProdutoService;
 import com.hygorluciano.lojaderoupa.domain.service.ProdutoServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,5 +29,9 @@ public class ProdutoController {
     @PutMapping("/{id}")
     public ResponseEntity<?> atualizarProduto(@PathVariable Long id, @RequestBody AtualizarProdutoDto dto){
         return produtoService.atualizarProduto(id,dto);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletaProduto(@PathVariable Long id){
+        return produtoService.deletaProduto(id);
     }
 }
