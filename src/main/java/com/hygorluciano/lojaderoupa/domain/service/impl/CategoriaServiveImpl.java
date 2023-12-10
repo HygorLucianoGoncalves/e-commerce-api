e443297e7694945b9380309b847797d98ab3d764
Hygor Luciano <hygorluciano0@gmail.com>
2023-12-10 09:22:45 -0300

    colocando o @Transactional nas ServiceImpl

--- src/main/java/com/hygorluciano/lojaderoupa/domain/service/impl/CategoriaServiveImpl.java
+++ src/main/java/com/hygorluciano/lojaderoupa/domain/service/impl/CategoriaServiveImpl.java
@@ -14,11 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component
 @Slf4j
+@Transactional
 public class CategoriaServiveImpl implements CategoriaService {
 
     @Autowired
--- src/main/java/com/hygorluciano/lojaderoupa/domain/service/impl/PedidoServiceImpl.java
+++ src/main/java/com/hygorluciano/lojaderoupa/domain/service/impl/PedidoServiceImpl.java
@@ -18,12 +18,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 @Slf4j
+@Transactional
 public class PedidoServiceImpl implements PedidoService {
     @Autowired
     PedidoRepository pedidoRepository;
